e7939e2183b8ccac5c35ad6024f63a10528cf51d
Andrea Nosler <andinosler@gmail.com>
2022-03-02 00:31:36 -0500

    use links json and add daily flag

---
+++ links.js
@@ -0,0 +1,25 @@
+var links = [
+    [    
+        {"title":"Twitch", "url":"https://www.twitch.tv/", "flags":""},
+        {"title":"MikuDB", "url":"http://mikudb.moe/", "flags":""},
+        {"title":"VGMusic", "url":"https://downloads.khinsider.com/", "flags":""},
+    ],
+    [
+        {"title":"YouTube", "url":"https://www.youtube.com/", "flags":""},
+        {"title":"YT Music", "url":"https://music.youtube.com/", "flags":""},
+        {"title":"Lofi Cafe", "url":"https://www.lofi.cafe/", "flags":""},
+        {"title":"Soundcloud", "url":"https://soundcloud.com/", "flags":""},    
+    ],
+    [
+        {"title":"GitHub", "url":"https://github.com/", "flags":""},
+        {"title":"Chase", "url":"https://www.chase.com/", "flags":""},
+        {"title":"Paypal", "url":"https://www.paypal.com/myaccount/summary", "flags":""},    
+        {"title":"Tumblr", "url":"https://www.tumblr.com/", "flags":""},
+    ],
+    [
+        {"title":"TinkerCAD", "url":"https://www.tinkercad.com/dashboard/", "flags":""},
+        {"title":"Inoreader", "url":"https://www.inoreader.com/", "flags":""},
+        {"title":"Goldfish", "url":"https://www.youtube.com/watch?v=gIM_OcyE5Tg", "flags":""},
+        {"title":"Gmail", "url":"https://mail.google.com/mail/u/0/", "flags":"daily"},    
+    ],
+];
