e54061d2e6a3c745dbb57229ef4a480ef260cc63
Andrea Nosler <andinosler@gmail.com>
2022-03-19 16:01:04 -0400

    update links + shorten box

--- links.js
+++ links.js
@@ -1,23 +1,23 @@
 var links = [
     [    
-        {"title":"Twitch", "url":"https://www.twitch.tv/", "flags":""},
+        {"title":"Pirate Bay", "url":"https://thepiratebay.org/index.html", "flags":""},
+        {"title":"Nyaa", "url":"https://www.nyaa.si/", "flags":""},
+        {"title":"Vimm's Lair", "url":"", "flags":"0"},
         {"title":"MikuDB", "url":"http://mikudb.moe/", "flags":""},
-        {"title":"VGMusic", "url":"https://downloads.khinsider.com/", "flags":""},
     ],
     [
+        {"title":"Twitch", "url":"https://www.twitch.tv/", "flags":""},
         {"title":"YouTube", "url":"https://www.youtube.com/", "flags":""},
         {"title":"YT Music", "url":"https://music.youtube.com/", "flags":""},
         {"title":"Lofi Cafe", "url":"https://www.lofi.cafe/", "flags":""},
-        {"title":"Soundcloud", "url":"https://soundcloud.com/", "flags":""},    
     ],
     [
         {"title":"GitHub", "url":"https://github.com/", "flags":""},
         {"title":"Chase", "url":"https://www.chase.com/", "flags":""},
         {"title":"Paypal", "url":"https://www.paypal.com/myaccount/summary", "flags":""},    
-        {"title":"Tumblr", "url":"https://www.tumblr.com/", "flags":""},
+        {"title":"TinkerCAD", "url":"https://www.tinkercad.com/dashboard/", "flags":""},
     ],
     [
-        {"title":"TinkerCAD", "url":"https://www.tinkercad.com/dashboard/", "flags":""},
         {"title":"Inoreader", "url":"https://www.inoreader.com/", "flags":""},
         {"title":"Goldfish", "url":"https://www.youtube.com/watch?v=gIM_OcyE5Tg", "flags":""},
         {"title":"Gmail", "url":"https://mail.google.com/mail/u/0/", "flags":"daily"},    
