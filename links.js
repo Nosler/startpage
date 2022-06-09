29d4baf72df8e056c19a10ca5efa765b10f86fe9
Nosler <andinosler@gmail.com>
2022-06-09 17:26:46 -0400

    refactor and add  big links

--- links.js
+++ links.js
@@ -1,25 +1,37 @@
+var biglinks = [
+    [    
+        {"title":"Reader", "url":"https://www.inoreader.com/", "flags":""},
+    ],
+    [        
+        {"title":"Email", "url":"https://mail.google.com/mail/u/0/", "flags":""},
+    ],
+    [        
+        {"title":"GitHub", "url":"https://github.com/Nosler", "flags":""},
+    ],
+    [       
+        {"title":"Calendar", "url":"https://calendar.google.com/calendar/u/0/r?tab=mc", "flags":""}, 
+    ],
+];
+
 var links = [
     [    
-        {"title":"Pirate Bay", "url":"https://thepiratebay.org/index.html", "flags":""},
-        {"title":"Nyaa", "url":"https://www.nyaa.si/", "flags":""},
-        {"title":"Vimm's Lair", "url":"", "flags":"0"},
-        {"title":"MikuDB", "url":"http://mikudb.moe/", "flags":""},
-    ],
-    [
-        {"title":"Twitch", "url":"https://www.twitch.tv/", "flags":""},
-        {"title":"YouTube", "url":"https://www.youtube.com/", "flags":""},
         {"title":"YT Music", "url":"https://music.youtube.com/", "flags":""},
-        {"title":"Lofi Cafe", "url":"https://www.lofi.cafe/", "flags":""},
+        {"title":"Jerma985", "url":"https://www.twitch.tv/jerma985", "flags":""},
+        {"title":"itch.io", "url":"https://itch.io/", "flags":""},
     ],
     [
-        {"title":"GitHub", "url":"https://github.com/", "flags":""},
-        {"title":"Chase", "url":"https://www.chase.com/", "flags":""},
-        {"title":"Paypal", "url":"https://www.paypal.com/myaccount/summary", "flags":""},    
-        {"title":"TinkerCAD", "url":"https://www.tinkercad.com/dashboard/", "flags":""},
+        {"title":"Letterboxd", "url":"https://letterboxd.com/", "flags":""},
+        {"title":"TinkerCAD", "url":"https://www.tinkercad.com/dashboard", "flags":""},
+        {"title":"Bank", "url":"https://www.chase.com/", "flags":""},
     ],
     [
-        {"title":"Inoreader", "url":"https://www.inoreader.com/", "flags":""},
-        {"title":"Goldfish", "url":"https://www.youtube.com/watch?v=gIM_OcyE5Tg", "flags":""},
-        {"title":"Gmail", "url":"https://mail.google.com/mail/u/0/", "flags":"daily"},    
+        {"title":"Rust Book", "url":"https://doc.rust-lang.org/book/", "flags":""},
+        {"title":"Rust Reference", "url":"https://doc.rust-lang.org/reference/index.html", "flags":""},
+        {"title":"The Bevy Book", "url":"https://bevyengine.org/learn/book/introduction/", "flags":""},
+    ],
+    [
+        {"title":"Jeet Kun Do", "url":"https://sites.google.com/centerlinestrong.com/jkd/phase-one?authuser=0https://sites.google.com/centerlinestrong.com/jkd/phase-one?authuser=0", "flags":""},
+        {"title":"Music Notes", "url":"https://www.notion.so/taylorlessonnotes/02f00fbb340a4be1b7ee899be921bc73?v=7b51a28883a04ab0a4857626f9fca403", "flags":""},
+        {"title":"Youtube Bevy", "url":"https://www.youtube.com/watch?v=WnUzWuaMzuM", "flags":""},
     ],
 ];
